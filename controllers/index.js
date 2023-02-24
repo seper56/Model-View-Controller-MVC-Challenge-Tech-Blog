18119d1f5a6e754e283557bda36ce780286759e0
semir <seper56@gmail.com>
2023-02-23 20:57:51 -0500

    ..

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const apiRoutes = require('./controllers');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 

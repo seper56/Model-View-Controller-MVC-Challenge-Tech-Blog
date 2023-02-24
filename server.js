a9ab142b38795a85638d6ec945c9d8b10f445012
semir <seper56@gmail.com>
2023-02-23 20:54:10 -0500

    ..

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.DB_SECRET,
+    secret: "process.env.DB_SECRET",
     cookie: {},
     resave: false,
     saveUninitialized: true,
